a58a10e999c2ba186da36b6d886498404118a1e1
eliasadmaso1 <eliasadmas01@gmail.com>
2022-07-07 04:36:33 +0300

    deploy

--- src/data/shirts.js
+++ src/data/shirts.js
@@ -9,9 +9,18 @@ import shirt8 from "../components/images/shirts/men/offwhite-blackB.jpeg";
 import shirt9 from "../components/images/shirts/men/replay-black.jpeg";
 
 import womanBarrowB from '../components/images/shirts/women/woman-barrowB.jpeg';
-import womanBarrow from '../components/images/shirts/women/woman-barrow.jpeg';
 import womanBarrow2 from '../components/images/shirts/women/woman-barrow2.jpeg';
-import womanBarrowB2 from '../components/images/shirts/women/woman-barrowB2.jpeg';
+import womanAdidas from '../components/images/shirts/women/woman-adidas.jpeg';
+import womanNikeFull from '../components/images/shirts/women/woman-nike-full.jpeg';
+import womanNikePro from '../components/images/shirts/women/woman-nikepro.jpeg';
+import womanNikeFullb from '../components/images/shirts/women/woman-nike-fullB.jpeg';
+import womanNikeFullc from '../components/images/shirts/women/woman-nike-fullC.jpeg';
+import womanNikeFulld from '../components/images/shirts/women/woman-nike-fullD.jpeg';
+import womanNikeFulle from '../components/images/shirts/women/woman-nike-fullE.jpeg';
+
+
+
+
 
 
 export const menShirts = [
@@ -84,30 +93,65 @@ export const womenPants = [
   {
     id: 1,
     name: "טי שירט עם הדפס לוגו",
-    img: womanBarrow,
+    img: womanBarrow2,
 
     price: "99.90 ₪",
 sizes:["S","M","L","XL"]  },
   {
     id: 2,
     name: "טי שירט עם הדפס לוגו",
-    img: womanBarrow2,
+    img: womanBarrowB,
 
     price: "99.90 ₪",
 sizes:["S","M","L","XL"]  },
   {
     id: 3,
     name: "טי שירט עם הדפס לוגו",
-    img: womanBarrowB,
+    img: womanAdidas,
 
     price: "99.90 ₪",
 sizes:["S","M","L","XL"]  },
   {
     id: 4,
     name: "טי שירט עם הדפס לוגו",
-    img: womanBarrowB2,
+    img: womanNikePro,
 
     price: "99.90 ₪",
+sizes:["S","M","L","XL"]  },
+{
+  id: 5,
+  name: "טי שירט עם הדפס לוגו",
+  img: womanNikeFull,
+
+  price: "99.90 ₪",
+sizes:["S","M","L","XL"]  },
+{
+  id: 6,
+  name: "טי שירט עם הדפס לוגו",
+  img: womanNikeFullb,
+
+  price: "99.90 ₪",
+sizes:["S","M","L","XL"]  },
+{
+  id: 7,
+  name: "טי שירט עם הדפס לוגו",
+  img: womanNikeFullc,
+
+  price: "99.90 ₪",
+sizes:["S","M","L","XL"]  },
+{
+  id: 8,
+  name: "טי שירט עם הדפס לוגו",
+  img: womanNikeFulld,
+
+  price: "99.90 ₪",
+sizes:["S","M","L","XL"]  },
+{
+  id: 9,
+  name: "טי שירט עם הדפס לוגו",
+  img: womanNikeFulle,
+
+  price: "99.90 ₪",
 sizes:["S","M","L","XL"]  },
 
 ]
