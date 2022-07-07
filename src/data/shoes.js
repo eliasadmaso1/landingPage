0cf81bcdf4310273e66606590551155dae21759c
eliasadmaso1 <eliasadmas01@gmail.com>
2022-07-07 05:07:27 +0300

    deploy

--- src/data/shoes.js
+++ src/data/shoes.js
@@ -1,6 +1,19 @@
+// men
+
+
 import nikeBlack from '../components/images/shoes/men/nike-black.jpeg';
 import nikeWhite from '../components/images/shoes/men/nike-white.jpeg';
 import nikeBlackGold from '../components/images/shoes/men/nike-blackgold.jpeg';
+
+import adidas from '../components/images/shoes/men/adidas-white.jpeg';
+import adidas1 from '../components/images/shoes/men/adidas1.jpeg';
+import adidas2 from '../components/images/shoes/men/adidas2.jpeg';
+import adidas3 from '../components/images/shoes/men/adidas3.jpeg';
+
+
+
+// women
+
 
 import karlBlackPink from '../components/images/shoes/women/karl-blackpink.jpeg';
 import karlBlackWhite from '../components/images/shoes/women/karl-blackwhite.jpeg';
@@ -13,6 +26,7 @@ import nike1 from '../components/images/shoes/women/nike-whiteblack.jpeg';
 import nike2 from '../components/images/shoes/women/nike-whiteyellow.jpeg';
 import nike3 from '../components/images/shoes/women/nike-whitepink.jpeg';
 import nike4 from '../components/images/shoes/women/nike-blackpink.jpeg';
+import { womenPants } from './shirts';
 
 
 
@@ -25,29 +39,62 @@ import nike4 from '../components/images/shoes/women/nike-blackpink.jpeg';
 
 
 export const menShoes = [
-    {
-        id: 1,
-        name: "טי שירט עם הדפס לוגו",
-        img: nikeBlack,
-        price: "99.90 ₪",
+//     {
+//         id: 1,
+//         name: "טי שירט עם הדפס לוגו",
+//         img: nikeBlack,
+//         price: "99.90 ₪",
+// sizes:[
+//     41,42,43,44
+// ]      },
+//       {
+//         id: 2,
+//         name: "טי שירט עם הדפס לוגו",
+//         img: nikeWhite,
+//         price: "99.90 ₪",
+// sizes:[
+//     41,42,43,44,45,46
+// ]      },
+//       {
+//         id: 3,
+//         name: "טי שירט עם הדפס לוגו",
+//         img: nikeBlackGold,
+//         price: "99.90 ₪",
+// sizes:[
+//     41,42,43,44
+// ]      },
+{
+    id: 4,
+    name: "טי שירט עם הדפס לוגו",
+    img: adidas,
+    price: "99.90 ₪",
 sizes:[
-    41,42,43,44
+41,42,43,44
+]      },   {
+    id: 5,
+    name: "טי שירט עם הדפס לוגו",
+    img: adidas1,
+    price: "99.90 ₪",
+sizes:[
+41,42,43,44
 ]      },
-      {
-        id: 1,
-        name: "טי שירט עם הדפס לוגו",
-        img: nikeWhite,
-        price: "99.90 ₪",
+
+{
+    id: 6,
+    name: "טי שירט עם הדפס לוגו",
+    img: adidas2,
+    price: "99.90 ₪",
 sizes:[
-    41,42,43,44,45,46
+41,42,43,44
 ]      },
-      {
-        id: 1,
-        name: "טי שירט עם הדפס לוגו",
-        img: nikeBlackGold,
-        price: "99.90 ₪",
+
+{
+    id: 7,
+    name: "טי שירט עם הדפס לוגו",
+    img: adidas3,
+    price: "99.90 ₪",
 sizes:[
-    41,42,43,44
+41,42,43,44
 ]      },
    
 
